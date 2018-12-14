3dce64b247e388f5232f7dc4b356be86f832f80a
周洋 <952827867@qq.com>
2018-12-14 17:52:52 +0800

    完善了xls和xlsx的读取问题，将其写入类excel_elements中，将excel_elements放入到List中

--- ZHY_ZY/src/Omap_Location/ReadExcel.java
+++ ZHY_ZY/src/Omap_Location/ReadExcel.java
@@ -1,76 +1,127 @@
 package Omap_Location;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+
+import javax.swing.JFileChooser;
 
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
-
-
-
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 
 public class ReadExcel {
 	
-	ReadExcel(){
-		read();
+	public static void main(String[] args){
+		JFileChooser jfc=new JFileChooser();
+		jfc.setFileSelectionMode(JFileChooser.FILES_ONLY);
+		jfc.showDialog(jfc, null);
+		File excel=jfc.getSelectedFile();
+		new ReadExcel().getExcel(excel);
 	}
 	
-	public static List<excel_elements> read(){
+	public void getExcel(File excel)
+	{
+		List<excel_elements> list=new ArrayList<excel_elements>();
+		if(excel.getName().endsWith("xls"))
+		{
+			list=Read_xls(excel);
+		}
+		else if(excel.getName().endsWith("xlsx"))
+		{
+			list=Read_xlsx(excel);
+		}
+		else
+		{
+			System.err.println("本程序不支持打开文件类型，"+excel.getName());
+		}
+		Iterator<excel_elements> iter=list.iterator();
+		while(iter.hasNext())
+		{
+			System.out.println(iter.next().CompanyName);
+		}
+		System.out.println(excel.length());
+	}
+	
+	
+	
+	public List<excel_elements> Read_xls(File excel)
+	{
+		List<excel_elements> excel_info=null;
+		
 		InputStream is=null;
-		HSSFWorkbook excel=null;
-		excel_elements ee=null;
+		HSSFWorkbook wb=null;
+		HSSFSheet sheet=null;
+		HSSFRow row=null;
+		Cell cell=null;
 		try {
-			is=new FileInputStream("C:\\Users\\周洋\\Desktop\\邾城普查企业名录.xls");
-			excel=new HSSFWorkbook(is);
-			List<excel_elements> list=new ArrayList<excel_elements>();
-			//循环工作表
-			for(int numSheet=0;numSheet<excel.getNumberOfSheets();numSheet++)
+			is=new FileInputStream(excel);
+			wb=new HSSFWorkbook(is);
+			excel_info=new ArrayList<excel_elements>();
+			sheet=wb.getSheet(wb.getSheetName(0));
+			for(int RowNum=0;RowNum<sheet.getLastRowNum();RowNum++)
 			{
-				HSSFSheet sheet=excel.getSheetAt(numSheet);
-				if(sheet==null)
+				row=sheet.getRow(RowNum);
+				if(row==null)
 					continue;
-				//循环行Row
-				for(int rowNum=1;rowNum<sheet.getLastRowNum();rowNum++)
-				{
-					HSSFRow row=sheet.getRow(rowNum);
-					if(row==null)
-						continue;
-					for(int cIndex=row.getFirstCellNum();cIndex<row.getLastCellNum();cIndex++)
-					{
-						ee=new excel_elements();
-						Cell cell=row.getCell(rowNum);
-						if(cell!=null)
-							System.out.print(cell+"\t");
-					}
-					if(row!=null)
-						System.out.println();
-					
-				}
+				excel_elements elements=new excel_elements();
+				elements.setName(row.getCell(1).toString(),row.getCell(2).toString(),row.getCell(3).toString(),row.getCell(4).toString());
+				excel_info.add(elements);
 			}
 			
-			
-			
-			
-			
 		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		return excel_info;
+	}
+	
+	public List<excel_elements> Read_xlsx(File excel)
+	{
+		List<excel_elements> excel_info=null;
 		
-		
-		
-		
-		
-		return null;
+		InputStream is=null;
+		XSSFWorkbook wb=null;
+		XSSFSheet sheet=null;
+		XSSFRow row=null;
+		Cell cell=null;
+		try {
+			is=new FileInputStream(excel);
+			wb=new XSSFWorkbook(is);
+			excel_info=new ArrayList<>();
+			sheet=wb.getSheet(wb.getSheetName(0));
+			for(int RowNum=0;RowNum<sheet.getLastRowNum();RowNum++)
+			{
+				row=sheet.getRow(RowNum);
+				if(row==null)
+					continue;
+				excel_elements elements=new excel_elements();
+				
+				elements.setName(row.getCell(1).toString(),row.getCell(2).toString(),row.getCell(3).toString(),row.getCell(4).toString());
+				excel_info.add(elements);	
+			}
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return excel_info;
 	}
 	
 
--- ZHY_ZY/src/Omap_Location/excel_elements.java
+++ ZHY_ZY/src/Omap_Location/excel_elements.java
@@ -2,9 +2,25 @@ package Omap_Location;
 
 public class excel_elements {
 	
+	//要从excel中获取的主要参数
+	String CompanyName;
+	String VillageName;
+	String VillageID;
+	String EnumeratorName;
+	String EnumeratorID;
+	String InstructorName;
+	String InstructorID;
+	String Longitude;
+	String latitude;
+	String seal_level;
 	
-	
-	
+	public void setName(String CompanyName,String VillageName,String Longitude,String latitude)
+	{
+		this.CompanyName=CompanyName;
+		this.VillageName=VillageName;
+		this.Longitude=Longitude;
+		this.latitude=latitude;
+	}
 	
 
 }
