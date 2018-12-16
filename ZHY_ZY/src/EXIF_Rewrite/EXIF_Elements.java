59e38f71037e71d34071ee4b88333a00516819d5
周洋 <952827867@qq.com>
2018-12-16 22:07:45 +0800

    获取exif，但是只能针对小米设备，还在思考怎么解决这个问题

--- ZHY_ZY/src/EXIF_Rewrite/EXIF_Elements.java
+++ ZHY_ZY/src/EXIF_Rewrite/EXIF_Elements.java
@@ -92,6 +92,14 @@ public class EXIF_Elements {
 
 	}
 	
+	public void setFileValues(String[] tag_info){
+		this.FileName=tag_info[0];
+		this.FileSize=tag_info[1];
+		this.FileModifiedDate=tag_info[2];
+
+	}
+	
+	
 	public void setExifIFD0Values(String  Model  ,String  Software  ,String  Orientation  ,String  DateTime  ,String  YCbCr_Positioning  ,String  ResolutionUnit  ,String  X_Resolution  ,String  Y_Resolution  ,String  Make){
 		this.Model=  Model;
 		this.Software=  Software;
@@ -102,6 +110,18 @@ public class EXIF_Elements {
 		this.X_Resolution =  X_Resolution;
 		this.Y_Resolution =  Y_Resolution;
 		this.Make =  Make;
+	}
+	
+	public void setExifIFD0Values(String[] tag_info){
+		this.Model=  tag_info[0];
+		this.Software=  tag_info[1];
+		this.Orientation=  tag_info[2];
+		this.DateTime=  tag_info[3];
+		this.YCbCr_Positioning=  tag_info[4];
+		this.ResolutionUnit=  tag_info[5];
+		this.X_Resolution =  tag_info[6];
+		this.Y_Resolution =  tag_info[7];
+		this.Make =  tag_info[8];
 	}
 	
 	public void setExifSubIFDValues(String  ISO_SpeedRatings  ,String  ExposureProgram  ,String  F_Number  ,String  ExposureTime  ,String  UnknownTag   ,String  SensingMethod  ,String  Sub_SecTimeDigitized  ,String  Sub_SecTimeOriginal  ,String  Sub_SecTime  ,String  FocalLength  ,String  Flash  ,String  MeteringMode  ,String  SceneCaptureType  ,String  FocalLength35  ,String  DateTimeDigitized  ,String  ExifImageHeight  ,String  WhiteBalanceMode  ,String  DateTimeOriginal  ,String  BrightnessValue  ,String  ExifImageWidth  ,String  ExposureMode  ,String  ApertureValue  ,String  ComponentsConfiguration  ,String  ColorSpace  ,String  SceneType  ,String  ShutterSpeedValue  ,String  ExifVersion  ,String  FlashPixVersion ){
@@ -135,6 +155,37 @@ public class EXIF_Elements {
 		this.FlashPixVersion =  FlashPixVersion;
 	}
 	
+	public void setExifSubIFDValues(String[] tag_info){
+		this.ISO_SpeedRatings =  tag_info[0];
+		this.ExposureProgram =  	tag_info[1];
+		this.F_Number =  	tag_info[2];
+		this.ExposureTime =  	tag_info[3];
+		this.UnknownTag =  	tag_info[4];
+		this.SensingMethod =  	tag_info[5];
+		this.Sub_SecTimeDigitized=  tag_info[6];
+		this.Sub_SecTimeOriginal =  	tag_info[7];
+		this.Sub_SecTime =  	tag_info[8];
+		this.FocalLength=  	tag_info[9];
+		this.Flash =  tag_info[10];
+		this.MeteringMode =  	tag_info[11];
+		this.SceneCaptureType =  	tag_info[12];
+		this.FocalLength35 =  tag_info[13];
+		this.DateTimeDigitized =  	tag_info[14];
+		this.ExifImageHeight  =	tag_info[15];
+		this.WhiteBalanceMode =  	tag_info[16];
+		this.DateTimeOriginal =  	tag_info[17];
+		this.BrightnessValue =  tag_info[18];
+		this.ExifImageWidth =  tag_info[19];
+		this.ExposureMode =  tag_info[20];
+		this.ApertureValue =  tag_info[21];
+		this.ComponentsConfiguration =  tag_info[22];
+		this.ColorSpace =  tag_info[23];
+		this.SceneType =  tag_info[24];
+		this.ShutterSpeedValue =  	tag_info[25];
+		this.ExifVersion =  	tag_info[26];
+		this.FlashPixVersion =  tag_info[27];
+	}
+	
 	public void setGPSValues(String  GPSLatitudeRef  ,String  GPSLatitude  ,String GPSLongitudeRef  ,String  GPSLongitude  ,String  GPSAltitudeRef  ,String  GPSAltitude  ,String  GPSTimeStamp  ,String  GPSProcessingMethod  ,String  GPSDateStamp){
 		this.GPSLatitudeRef =  GPSLatitudeRef  ;
 		this.GPSLatitude =  GPSLatitude  ;
@@ -145,6 +196,18 @@ public class EXIF_Elements {
 		this.GPSTimeStamp =  GPSTimeStamp  ;
 		this.GPSProcessingMethod =  GPSProcessingMethod  ;
 		this.GPSDateStamp =  GPSDateStamp  ;
+	}
+	
+	public void setGPSValues(String[] tag_info){
+		this.GPSLatitudeRef =  tag_info[0]  ;
+		this.GPSLatitude =   tag_info[1]   ;
+		this.GPSLongitudeRef =   tag_info[2]   ;
+		this.GPSLongitude =   tag_info[3]   ;
+		this.GPSAltitudeRef =   tag_info[4]   ;
+		this.GPSAltitude =   tag_info[5]   ;
+		this.GPSTimeStamp =   tag_info[6]   ;
+		this.GPSProcessingMethod =   tag_info[7]   ;
+		this.GPSDateStamp =   tag_info[8]   ;
 	}
 	
 	public void setJPEGValues(String  CompressionType  ,String  DataPrecision  ,String  ImageHeight  ,String  ImageWidth  ,String  NumberofComponents  ,String  Component1  ,String  Component2  ,String  Component3){
@@ -158,9 +221,25 @@ public class EXIF_Elements {
 		this.Component3 =  Component3  ;
 	}
 	
+	public void setJPEGValues(String[] tag_info){
+		this.CompressionType =  tag_info[0]  ;
+		this.DataPrecision =  tag_info[1]   ;
+		this.ImageHeight =  tag_info[2]   ;
+		this.ImageWidth =  tag_info[3]   ;
+		this.NumberofComponents =  tag_info[4]   ;
+		this.Component1 =  tag_info[5]   ;
+		this.Component2 =  tag_info[6]   ;
+		this.Component3 =  tag_info[7]   ;
+	}
+	
 	public void setInteroperabilityValues(String  InteroperabilityIndex  ,String  InteroperabilityVersion ){
 		this.InteroperabilityIndex =  InteroperabilityIndex  ;
 		this.InteroperabilityVersion =  InteroperabilityVersion  ;
+	}
+	
+	public void setInteroperabilityValues(String[] tag_info ){
+		this.InteroperabilityIndex =  tag_info[0]  ;
+		this.InteroperabilityVersion =  tag_info[1]  ;
 	}
 	
 	public void setExifThumbnailValues(String  ThumbnailOffset  ,String  Thumbnail_Orientation  ,String  ThumbnailLength  ,String  ThumbnailCompression  ,String  Thumbnail_ResolutionUnit  ,String  Thumbnail_X_Resolution  ,String  Thumbnail_Y_Resolution){
@@ -171,6 +250,16 @@ public class EXIF_Elements {
 		this.Thumbnail_ResolutionUnit =  Thumbnail_ResolutionUnit  ;
 		this.Thumbnail_X_Resolution =  Thumbnail_X_Resolution  ;
 		this.Thumbnail_Y_Resolution =  Thumbnail_Y_Resolution  ;
+	}
+	
+	public void setExifThumbnailValues(String[] tag_info){
+		this.ThumbnailOffset =  tag_info[0]  ;
+		this.Thumbnail_Orientation =   tag_info[1]   ;
+		this.ThumbnailLength =   tag_info[2]   ;
+		this.ThumbnailCompression =   tag_info[3]   ;
+		this.Thumbnail_ResolutionUnit =   tag_info[4]   ;
+		this.Thumbnail_X_Resolution =   tag_info[5]   ;
+		this.Thumbnail_Y_Resolution =   tag_info[6]   ;
 	}
 
 }
--- ZHY_ZY/src/EXIF_Rewrite/JPG_EXIF.java
+++ ZHY_ZY/src/EXIF_Rewrite/JPG_EXIF.java
@@ -15,7 +15,7 @@ public class JPG_EXIF {
 	
 	public static void main(String[] args)
 	{
-		File f=new File("C:\\Users\\周洋\\Desktop\\新洲污普\\新建文件夹\\IMG_20180911_110838.jpg");
+		File f=new File("C:\\Users\\周洋\\Desktop\\QQ图片20181216215623.jpg");
 		new JPG_EXIF().getEXIF(f);
 	}
 	
@@ -29,8 +29,9 @@ public class JPG_EXIF {
 				
 				for(Tag tag:directory.getTags())
 				{
-						System.out.println(tag);
+						System.out.println(tag.getDirectoryName()+"     "+tag.getTagName()+"     "+tag.getDescription());
 				}
+				System.out.println();
 			}
 			
 		} catch (JpegProcessingException | IOException e) {
@@ -39,57 +40,79 @@ public class JPG_EXIF {
 		}
 	}
 
-	public List<EXIF_Elements> getEXIF(File jpegFile)
+	public void getEXIF(File jpegFile)
 	{
-		List<EXIF_Elements> list=new ArrayList<EXIF_Elements>();
+	//	List<EXIF_Elements> list=new ArrayList<EXIF_Elements>();
 		Metadata metadata=null;
+		EXIF_Elements elements=new EXIF_Elements();
 		try {
 			metadata=JpegMetadataReader.readMetadata(jpegFile);
 			for(Directory directory:metadata.getDirectories())
 			{
-				EXIF_Elements elements=new EXIF_Elements();
 				int index=directory.getTags().size();
+				System.out.println(directory.getName()+"\tindex="+index);
 				String[] tag_info=new String[index];
 				index=0;
-				System.out.print("public void set"+directory.getName()+"Values(");
+	//			System.out.print("public void set"+directory.getName()+"Values(");
 				for(Tag tag:directory.getTags())
 				{
 					tag_info[index]=tag.getDescription();
 					index++;
-					System.out.print("String  "+tag.getTagName()+"  ,");
+	//				System.out.print("String  "+tag.getTagName()+"  ,");
 				}
 				
 				if(directory.getName().contains("File")){
-					elements.setFileValues(FileName, FileSize, FileModifiedDate);
+					elements.setFileValues(tag_info);
 				}
 				if(directory.getName().contains("Exif IFD0")){
-					
+					elements.setExifIFD0Values(tag_info);;
 				}
 				if(directory.getName().contains("SubIFD")){
-					
+					elements.setExifSubIFDValues(tag_info);
 				}
 				if(directory.getName().contains("GPS")){
-					
+					elements.setGPSValues(tag_info);
 				}
 				if(directory.getName().contains("JPEG")){
-					
+					elements.setJPEGValues(tag_info);
 				}
 				if(directory.getName().contains("Interoperability")){
-					
+					elements.setInteroperabilityValues(tag_info);
 				}
 				if(directory.getName().contains("Thumbnail")){
-					
+					elements.setExifThumbnailValues(tag_info);
 				}
-				
-				System.out.print("){\n\n}\n\n\n");
+		//		System.out.print("){\n\n}\n\n\n");
 			}
 		} catch (JpegProcessingException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		return list;
+		System.out.println(elements.GPSDateStamp);
+	//	return null;
+	}
+	
+	public void readFile(File file)
+	{
+		if(file.isDirectory())
+		{
+			File[] f=file.listFiles();
+			for(int index=0;index<f.length;index++)
+			{
+				if(f[index].isFile())
+				{
+					if(f[index].getName().endsWith(".jpg")|f[index].getName().endsWith(".jpeg"))
+					{
+						getEXIF(f[index]);
+					}
+				}
+				else if(f[index].isDirectory())
+				{
+					
+				}
+			}
+		}
 	}
 	
 }
