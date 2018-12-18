2c78c48d10d3fa6011b62ec6784c829cf5936315
周洋 <952827867@qq.com>
2018-12-18 13:53:53 +0800

    get GPS, write to xls

--- ZHY_ZY/src/Omap_Location/JPG_EXIF.java
+++ ZHY_ZY/src/Omap_Location/JPG_EXIF.java
@@ -1,7 +1,11 @@
 package Omap_Location;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+
+import javax.swing.JFileChooser;
 
 import com.drew.imaging.jpeg.JpegMetadataReader;
 import com.drew.imaging.jpeg.JpegProcessingException;
@@ -13,19 +17,24 @@ public class JPG_EXIF {
 	
 	public static void main(String[] args)
 	{
-		File f=new File("E:\\新洲污普\\新建文件夹\\IMG_20180911_110902.jpg");
-		new JPG_EXIF().readEXIF(f);
+		JFileChooser jfc =new JFileChooser();
+		jfc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+		jfc.showDialog(null, null);
+		File f=jfc.getSelectedFile();
+		new JPG_EXIF().readFolder(f);
 	}
 	
 	
 	public void readEXIF(File jpegFile)
 	{
+		String tag_info="";
 		try {
 			Metadata metadata=JpegMetadataReader.readMetadata(jpegFile);
 			for(Directory directory:metadata.getDirectories())
 			{
 				for(Tag tag:directory.getTags())
 				{
+					tag_info+=tag.getDescription()+"\t";
 						System.out.println(tag);
 				}
 			}
@@ -34,6 +43,50 @@ public class JPG_EXIF {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		write(tag_info);
 	}
 
+	public void write(String taginfo)
+	{
+		try {
+			FileWriter fw=new FileWriter("D:\\照片信息.xls",true);
+			BufferedWriter bw=new BufferedWriter(fw);
+			bw.write(taginfo);
+			bw.newLine();
+			bw.flush();
+			bw.close();
+			fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void readFolder(File file)
+	{
+		if(file.isFile())
+		{
+			if(file.getName().endsWith("jpg")|file.getName().endsWith("jpeg"))
+			{
+				readEXIF(file);
+			}
+		}else if(file.isDirectory())
+		{
+			File[] path=file.listFiles();
+			for(int i=0;i<path.length;i++)
+			{
+				if(path[i].isFile())
+				{
+					if(path[i].getName().endsWith("jpg")|path[i].getName().endsWith("jpeg"))
+					{
+						readEXIF(path[i]);
+					}
+				}else if(path[i].isDirectory())
+				{
+					readFolder(path[i]);
+				}
+			}
+		}
+	}
+	
+	
 }
