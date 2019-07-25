3e9034e6edf8b58f42128c1e1881735eb6caaddb
zhouyang <zhouyang@192.168.31.104>
2019-07-25 23:00:21 +0800

    1

---
+++ ZHY_ZY/src/demo/getFilePath.java
@@ -0,0 +1,44 @@
+package demo;
+
+import java.io.File;
+
+import javax.swing.JFileChooser;
+import javax.swing.JLabel;
+
+public class getFilePath {
+	
+	public File getFile()
+	{
+		JFileChooser jfc=new JFileChooser();
+		jfc.setFileSelectionMode(JFileChooser.FILES_ONLY);
+		jfc.showDialog(new JLabel(),"打开");
+		File file=jfc.getSelectedFile();
+		return file;
+	}
+	public String getFileString()
+	{
+		JFileChooser jfc=new JFileChooser();
+		jfc.setFileSelectionMode(JFileChooser.FILES_ONLY);
+		jfc.showDialog(new JLabel(),"打开");
+		File file=jfc.getSelectedFile();
+		return file.toString();
+	}
+	
+	public File getFolder()
+	{
+		JFileChooser jfc=new JFileChooser();
+		jfc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		jfc.showDialog(new JLabel(),"打开");
+		File file=jfc.getSelectedFile();
+		return file;
+	}
+	public String getFolderString()
+	{
+		JFileChooser jfc=new JFileChooser();
+		jfc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		jfc.showDialog(new JLabel(),"打开");
+		File file=jfc.getSelectedFile();
+		return file.toString();
+	}
+	
+}
